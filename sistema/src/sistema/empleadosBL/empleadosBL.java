ab3c30ff933b0f6b584512bb2c5cb2b841c580f1
6SKP9 <higuitaeda@gmail.com>
2022-05-04 21:24:22 -0500

    ninguna cambio aun

---
+++ sistema/src/sistema/empleadosBL/empleadosBL.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistema.empleadosBL;
+
+/**
+ *Creamos los métodos setters y getters de cada elemento
+ * @author Daniel
+ */
+public class empleadosBL {
+    
+    int ID, cel;
+String nombre, correo;
+    public int getCel() {
+        return cel;
+    }
+
+    public void setCel(int cel) {
+        this.cel = cel;
+    }
+    
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+    
+}
